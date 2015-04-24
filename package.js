01af39c21d170d7e774f638f951d484125cfa434
hipstersmoothie <lisowski54@gmail.com>
2015-04-23 20:58:20 -0700

    New version

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name : "hipstersmoothie:scrollmagic",
   summary: "ScrollMagic repackaged for Meteor.",
-  version: "0.0.2",
+  version: "0.0.3",
   git: " https://github.com/hipstersmoothie/Meteor-ScrollMagic"
 });
 
